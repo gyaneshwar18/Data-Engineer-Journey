4b17fd0986f72314a5f97f9208916026590a4ece
gyaneshwar18 <suryavanshigyaneshwar18@gmail.com>
2025-11-22 23:14:06 +0530

    organize files

--- Day07_File_Organizer_Logger/file_organizer.py
+++ Day07_File_Organizer_Logger/file_organizer.py
@@ -9,9 +9,26 @@ BASE_DIR = Path("sample_files")
 TARGET_DIR = Path("organized_files")
 LOG_FILE = Path("logs/file_log.txt")
 
+
 # ---------- LOGGING CONFIG ----------
 logging.basicConfig(
     filename=LOG_FILE,
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(message)s",
 )
+
+# ---------- FUNCTION DEFINITIONS ----------
+def organize_files():
+    """Organize files by extension into folders"""
+    if not TARGET_DIR.exists():
+        TARGET_DIR.mkdir(parents=True)
+
+    for file in BASE_DIR.iterdir():
+        if file.is_file():
+            ext = file.suffix.replace('.', '')
+            folder = TARGET_DIR / ext.upper()
+            folder.mkdir(exist_ok=True)
+
+            destination = folder / file.name
+            shutil.move(str(file), str(destination))
+            logging.info(f"Moved {file.name} → {folder}")
