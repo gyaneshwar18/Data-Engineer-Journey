357feb75b8590950a64b0ace51e6dd2f1990c2d3
gyaneshwar18 <suryavanshigyaneshwar18@gmail.com>
2025-11-27 23:41:37 +0530

    Opening

--- Day08_Log_Analyzer/log_analyzer.py
+++ Day08_Log_Analyzer/log_analyzer.py
@@ -7,3 +7,18 @@ LOG_FILE = Path("logs/file_log.txt")
 
 # Regex pattern to extract timestamp, level, file, folder
 pattern = r"^(.*?) \| (\w+) \| Moved (.*?) → (.*)$"
+
+data = []
+
+with open(LOG_FILE, "r") as f:
+    for line in f:
+        match = re.match(pattern, line.strip())
+        if match:
+            timestamp, level, filename, folder = match.groups()
+            data.append([timestamp, level, filename, folder])
+
+# Create DataFrame
+df = pd.DataFrame(data, columns=["timestamp", "level", "filename", "folder"])
+
+print("Extracted Log Data:")
+print(df, "\n")
