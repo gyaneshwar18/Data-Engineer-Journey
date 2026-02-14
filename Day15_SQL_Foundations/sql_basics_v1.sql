033d3ec51d6b22393f5fac72ccbecb00e8a2a382
gyaneshwar18 <suryavanshigyaneshwar18@gmail.com>
2026-02-14 19:09:28 +0530

    SQL Basics

---
+++ Day15_SQL_Foundations/sql_basics_v1.sql
@@ -0,0 +1,24 @@
+CREATE TABLE employees (
+    emp_id INT,
+    name TEXT,
+    department TEXT,
+    salary INT,
+    city TEXT
+);
+
+INSERT INTO employees VALUES
+(1,'Riya','Data',60000,'Hyderabad'),
+(2,'Aryan','IT',50000,'Mumbai'),
+(3,'Sneha','Data',75000,'Hyderabad'),
+(4,'Rahul','HR',45000,'Delhi'),
+(5,'Neha','IT',70000,'Mumbai');
+
+SELECT * FROM employees;
+
+SELECT name, salary
+FROM employees
+WHERE salary > 60000;
+
+SELECT *
+FROM employees
+ORDER BY salary DESC;
