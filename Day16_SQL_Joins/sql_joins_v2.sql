79b792088fe51f519a010a1649b14df7c39439a7
gyaneshwar18 <suryavanshigyaneshwar18@gmail.com>
2026-02-17 22:22:53 +0530

    Joins

--- Day16_SQL_Joins/sql_joins_v2.sql
+++ Day16_SQL_Joins/sql_joins_v2.sql
@@ -15,3 +15,36 @@ CREATE TABLE IF NOT EXISTS orders (
     customer_id INT,
     amount INT
 );
+INSERT INTO orders VALUES
+(101,1,500),
+(102,1,700),
+(103,2,300),
+(104,5,900);
+
+SELECT c.name, o.amount
+FROM customers c
+INNER JOIN orders o
+ON c.customer_id = o.customer_id;
+
+# LEFT JOIN
+
+SELECT c.name, o.amount
+FROM customers c
+LEFT JOIN orders o
+ON c.customer_id = o.customer_id;
+
+# Find Customers Without Orders
+Select c.name
+FROM customers c
+LEFT JOIN orders o
+ON c.customer_id = o.customer_id
+WHERE o.order_id IS NULL;
+
+# Find Customers Without Orders
+
+SELECT c.name,
+       o.amount AS total_spend
+FROM customers c
+JOIN orders o
+ON c.customer_id = o.customer_id
+GROUP BY c.name;
