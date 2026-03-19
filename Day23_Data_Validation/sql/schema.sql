04bad5f6cb52f6d895e52c70ea24e93691696891
gyaneshwar18 <suryavanshigyaneshwar18@gmail.com>
2026-03-19 17:33:48 +0530

    Schema fixed

--- Day23_Data_Validation/sql/schema.sql
+++ Day23_Data_Validation/sql/schema.sql
@@ -1,7 +1,6 @@
-CREATE TABLE IF NOT EXISTS rejected_orders (
-    customer_id TEXT,
-    order_date TEXT,
-    amount TEXT,
-    status TEXT,
-    reason TEXT
+CREATE TABLE IF NOT EXISTS orders (
+    customer_id INT,
+    order_date DATE,
+    amount FLOAT,
+    status TEXT
 );
