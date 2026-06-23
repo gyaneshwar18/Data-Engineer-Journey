99e4a91cdf5564c08c58f390b58c1ed6b5a6cb2e
gyaneshwar18 <suryavanshigyaneshwar18@gmail.com>
2026-06-23 23:39:43 +0530

    ContainsDuplicates

---
+++ Coding Problems/ContainsDuplicate.py
@@ -0,0 +1,34 @@
+#using HashMap
+
+# n = int(input())
+# nums = list(map(int, input().split()))
+
+# freq = {}
+
+# for num in nums:
+#     freq[num] = freq.get(num, 0) + 1
+
+# for value in freq.values():
+#     if value > 1:
+#         print(True)
+#         break
+# else:
+#     print(False)
+
+#using set
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+seen = set()
+
+for num in nums:
+
+    if num in seen:
+        print(True)
+        break
+
+    seen.add(num)
+
+else:
+    print(False)
