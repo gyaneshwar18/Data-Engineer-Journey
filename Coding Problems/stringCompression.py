4d800995a0aa209cbf450ec098ef7cc96ea327d7
gyaneshwar18 <suryavanshigyaneshwar18@gmail.com>
2026-07-04 08:07:49 +0530

    StringCompression

---
+++ Coding Problems/stringCompression.py
@@ -0,0 +1,22 @@
+s = input()
+
+result = []
+
+i = 0
+
+while i < len(s):
+
+    count = 1
+
+    while i + 1 < len(s) and s[i] == s[i + 1]:
+        count += 1
+        i += 1
+
+    result.append(s[i])
+
+    if count > 1:
+        result.append(str(count))
+
+    i += 1
+
+print("".join(result))
