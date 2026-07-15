07ffda9a47961a2a6e39c61a6a37ccccce4da145
gyaneshwar18 <suryavanshigyaneshwar18@gmail.com>
2026-07-15 16:26:07 +0000

    Spark select(), filter(), withColumn()

--- Day48_PySpark/scripts/day51_dataframes.py
+++ Day48_PySpark/scripts/day51_dataframes.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import lit
 
 spark=(
 	SparkSession.builder
@@ -19,4 +20,24 @@ print("Showing data:")
 print("=" *50)
 
 df.show()
+
+print("=" *50)
+print("Selecting columns:")
+print("=" *50)
+
+df.select("customer_id", "name").show()
+
+print("=" *50)
+print("Adding Filtering:")
+print("=" *50)
+
+df.filter(df.age>27).show()
+
+print("=" *50)
+print("Adding columns:")
+print("=" *50)
+
+df.withColumn("Country", lit("India")).show()
+
+
 spark.stop()
